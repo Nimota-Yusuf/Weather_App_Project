869114bfd248feafcb4605ea2a851a47976374ad
Nimota <nimota2001@gmail.com>
2024-02-17 08:44:20 +0100

    Updated index.js with API functinality

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 function city(event){
     event.preventDefault();
     let form_input = document.querySelector("#form-input");
-    let h1Element = document.querySelector ('h1');
-    h1Element.innerHTML = form_input.value
+    searchCity(form_input.value);
 }
-
 let searchElement = document.querySelector("form");
 searchElement.addEventListener('submit', city);
+
+function searchCity(city){
+    let apiKey = "f8f60a003btf3df78oa2b71e8949ee2f";
+    let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+    axios.get(apiURL).then(display_temperature);
+}
+
+function display_temperature(response){
+   let temperature = response.data.temperature.current;
+   let temperature_result = document.querySelector('#temperature');
+   temperature_result.innerHTML = Math.round(temperature);
+   
+   let h1Element = document.querySelector("h1");
+   h1Element.innerHTML = response.data.city;
+}
